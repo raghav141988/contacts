ae753b867f85e04827b7924ef4557ae1fcf9e66b
raghav141988 <raghavendra.pes@gmail.com>
2019-11-14 16:57:59 -0600

    travis and deploy script added

---
+++ deploy.sh
@@ -0,0 +1,12 @@
+docker build -t raghav141988/contacts-frontend:latest -t raghav141988/contacts-frontend:$SHA -f ./contacts-frontend/Dockerfile ./contacts-frontend
+docker build -t raghav141988/contacts-backend:latest -t raghav141988/contacts-backend:$SHA -f ./contacts-backend/Dockerfile ./contacts-backend
+
+docker push raghav141988/contacts-backend:latest
+docker push raghav141988/contacts-frontend:latest
+
+docker push raghav141988/contacts-backend:$SHA
+docker push raghav141988/contacts-frontend:$SHA
+
+kubectl apply -f ./contacts-deployment/k8s
+kubectl set image deployments/contact-frontend-deployment contacts-frontend=raghav141988/contacts-backend:$SHA
+kubectl set image deployments/contact-backend-deployment contacts-api=raghav141988/contacts-backend:$SHA
