ccb1936a1cbe3b18dea25edd2305b9e2edcaaf55
raghav141988 <raghavendra.pes@gmail.com>
2019-11-16 08:44:13 -0600

    deploy

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,10 @@
 cd $TRAVIS_BUILD_DIR/contacts-backend/
 ls -l
 echo $PWD
-docker build -t raghav141988/contacts-backend:latest -t raghav141988/contacts-backend:$SHA -f ./contacts-backend/Dockerfile ./contacts-backend
+docker build -t raghav141988/contacts-backend:latest -t raghav141988/contacts-backend:$SHA -f ./Dockerfile .
+cd $TRAVIS_BUILD_DIR
+ls -l
+echo $PWD
 docker build -t raghav141988/contacts-frontend:latest -t raghav141988/contacts-frontend:$SHA -f ./contacts-frontend/Dockerfile ./contacts-frontend
 docker push raghav141988/contacts-backend:latest
 docker push raghav141988/contacts-frontend:latest
