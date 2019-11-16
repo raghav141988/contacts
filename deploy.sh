aac01ba4177cd0e83f5437428647c505c39dfcde
raghav141988 <raghavendra.pes@gmail.com>
2019-11-16 11:25:21 -0600

    build script changes

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,16 @@
 #!/bin/sh
-docker build -t raghav141988/contacts-backend:latest -t raghav141988/contacts-backend:$SHA -f ./contacts-backend/Dockerfile .
-docker build -t raghav141988/contacts-frontend:latest -t raghav141988/contacts-frontend:$SHA -f ./contacts-frontend/Dockerfile ./contacts-frontend
 
+
+cd $TRAVIS_BUILD_DIR/contacts-backend
+for file in $PWD/*; do
+    echo "$(basename "$file")"
+done
+
+docker build -t raghav141988/contacts-backend:latest -t raghav141988/contacts-backend:$SHA -f ./Dockerfile .
+cd $TRAVIS_BUILD_DIR
+ls -l
+echo $PWD
+docker build -t raghav141988/contacts-frontend:latest -t raghav141988/contacts-frontend:$SHA -f ./contacts-frontend/Dockerfile ./contacts-frontend
 docker push raghav141988/contacts-backend:latest
 docker push raghav141988/contacts-frontend:latest
 
