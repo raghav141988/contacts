f8f69ffe23a5e5151183dc7028a5e6d8340d6196
raghav141988 <raghavendra.pes@gmail.com>
2019-11-15 22:21:18 -0600

    contact front end image added

--- deploy.sh
+++ deploy.sh
@@ -8,5 +8,5 @@ docker push raghav141988/contacts-backend:$SHA
 docker push raghav141988/contacts-frontend:$SHA
 
 kubectl apply -f ./contacts-deployment/k8s
-kubectl set image deployments/contact-frontend-deployment contacts-frontend=raghav141988/contacts-backend:$SHA
+kubectl set image deployments/contact-frontend-deployment contacts-frontend=raghav141988/contacts-frontend:$SHA
 kubectl set image deployments/contact-backend-deployment contacts-api=raghav141988/contacts-backend:$SHA
