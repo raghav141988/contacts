a8fc0008a7bf554a6e577e43c7c4e484ef9e846f
raghav141988 <raghavendra.pes@gmail.com>
2019-11-18 17:07:12 -0600

    buildId added

--- deploy.sh
+++ deploy.sh
@@ -2,17 +2,17 @@
 
 
 cd $TRAVIS_BUILD_DIR/contacts-backend
-docker build -t raghav141988/contacts-backend:latest -t raghav141988/contacts-backend:$SHA -f ./Dockerfile .
+docker build -t raghav141988/contacts-backend:latest -t raghav141988/contacts-backend:$BUILD_ID -f ./Dockerfile .
 cd $TRAVIS_BUILD_DIR
 ls -l
 echo $PWD
-docker build -t raghav141988/contacts-frontend:latest -t raghav141988/contacts-frontend:$SHA -f ./contacts-frontend/Dockerfile ./contacts-frontend
+docker build -t raghav141988/contacts-frontend:latest -t raghav141988/contacts-frontend:$BUILD_ID -f ./contacts-frontend/Dockerfile ./contacts-frontend
 docker push raghav141988/contacts-backend:latest
 docker push raghav141988/contacts-frontend:latest
 
-docker push raghav141988/contacts-backend:$SHA
-docker push raghav141988/contacts-frontend:$SHA
+docker push raghav141988/contacts-backend:$BUILD_ID
+docker push raghav141988/contacts-frontend:$BUILD_ID
 
 kubectl apply -f ./contacts-deployment/k8s
-kubectl set image deployments/contact-frontend-deployment contacts-frontend=raghav141988/contacts-frontend:$SHA
-kubectl set image deployments/contact-backend-deployment contacts-api=raghav141988/contacts-backend:$SHA
+kubectl set image deployments/contact-frontend-deployment contacts-frontend=raghav141988/contacts-frontend:$BUILD_ID
+kubectl set image deployments/contact-backend-deployment contacts-api=raghav141988/contacts-backend:$BUILD_ID
