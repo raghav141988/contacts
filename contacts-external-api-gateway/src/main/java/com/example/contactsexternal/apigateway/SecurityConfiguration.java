c47a882252271b00621d557ecdaf280600f10bd0
raghav141988 <raghavendra.pes@gmail.com>
2019-12-02 17:48:05 -0600

    Prod security config changes

--- contacts-external-api-gateway/src/main/java/com/example/contactsexternal/apigateway/SecurityConfiguration.java
+++ contacts-external-api-gateway/src/main/java/com/example/contactsexternal/apigateway/SecurityConfiguration.java
@@ -1,25 +1,35 @@
 package com.example.contactsexternal.apigateway;
 
 import org.springframework.context.annotation.Profile;
-
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
+import lombok.extern.slf4j.Slf4j;
+
 
 
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true) 
 @Profile("!dev")
+@Slf4j
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
    
     @Override
     protected void configure(HttpSecurity http) throws Exception{
         http.csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse()).and().
-        authorizeRequests()
-        .antMatchers( "/**").permitAll();
-        http.oauth2ResourceServer();
+        antMatcher("/**")  
+        .authorizeRequests()  
+        .antMatchers(HttpMethod.OPTIONS, "/**").permitAll()
+        .antMatchers("/token/**").permitAll()  
+        .anyRequest().authenticated();
+		
+       
+		http.oauth2ResourceServer();
     }
     
     
